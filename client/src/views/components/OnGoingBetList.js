6d2d0b46626a3030c126be684902a1c00515f77a
AlulaLeak <alula.leakemariam94@gmail.com>
2022-02-15 19:52:23 -0500

    necessary information and basic components ready for design. Only early cashout is hard coded (until I bring information from the db to the client)

--- client/src/views/components/OnGoingBetList.js
+++ client/src/views/components/OnGoingBetList.js
@@ -1,6 +1,7 @@
 import React from "react";
 import OnGoingBetItem from "./OnGoingBetItem";
 import { useOngoingBets } from "../../helpers/useOnGoingBets";
+import "../../styles/OnGoingBetList.css";
 
 function OnGoingBetList() {
   const { usersOnGoingBets } = useOngoingBets();
@@ -9,7 +10,28 @@ function OnGoingBetList() {
     <>
       {usersOnGoingBets &&
         usersOnGoingBets.map((onGoingBetSlip, idx) => {
-          return <OnGoingBetItem key={idx} onGoingBetSlip={onGoingBetSlip} />;
+          return (
+            <>
+              <OnGoingBetItem key={idx} onGoingBetSlip={onGoingBetSlip} />
+              <div>
+                (break line - end of bet-slip - refer to wireframe in discord
+                design)
+                <div className="ongoing-bet-bottom">
+                  <div className="potential-payout-ongoing">
+                    <button>Early Cashout</button>
+                  </div>
+                  <div className="potential-payout-ongoing">
+                    <div>Potential Payout: </div>
+                    <div> {onGoingBetSlip[0].potential_payout}</div>
+                  </div>
+                  <div className="potential-payout-ongoing">
+                    <div>Amount Wagered: </div>
+                    <div> {onGoingBetSlip[0].amount_wagered}</div>
+                  </div>
+                </div>
+              </div>
+            </>
+          );
         })}
     </>
   );
